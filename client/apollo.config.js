231a72fece4a323c719f897e8a8309929a86cf29
Jessica Arrais <jessicalarrais@gmail.com>
2020-05-15 11:32:38 -0700

    client

---
+++ client/apollo.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'todo-app',
+      url: 'http://localhost:4000/',
+      // optional headers
+      // headers: {
+      //   authorization: 'Bearer lkjfalkfjadkfjeopknavadf',
+      // },
+      // // optional disable SSL validation check
+      skipSSLValidation: true,
+    },
+  },
+};
