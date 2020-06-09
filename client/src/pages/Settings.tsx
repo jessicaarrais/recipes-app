268e3fffd92a173955e4f60357ac36e3c86979a5
Jessica Arrais <jessicalarrais@gmail.com>
2020-06-09 11:44:34 -0700

    react router added to project

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useApolloClient } from '@apollo/react-hooks';
+import { useHistory } from 'react-router';
+import Button from '../components/Button';
 import DeleteUserButton from '../components/DeleteUserButton';
 
 function Settings() {
+  const client = useApolloClient();
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    client.cache.reset();
+    client.writeData({ data: { isLoggedIn: false } });
+    history.push('/');
+  };
+
+  const handleBackToHomeNavigation = () => history.push('/home');
+
   return (
     <>
+      <Button
+        type="button"
+        styleType="default"
+        handleOnClick={handleBackToHomeNavigation}
+        handleOnKeyDown={handleBackToHomeNavigation}
+      >
+        Back to Home
+      </Button>
       <DeleteUserButton />
+      <Button
+        type="button"
+        styleType="default"
+        handleOnClick={handleLogout}
+        handleOnKeyDown={(e) => {
+          if (e.key === 'Enter') handleLogout();
+        }}
+      >
+        Logout
+      </Button>
     </>
   );
 }
