5d28b3aa568068a8b509dce035b2cef7af5d2e1c
Jessica Arrais <jessicalarrais@gmail.com>
2020-08-20 13:02:27 -0700

    Updates

---
+++ precache-manifest.bc76ce90fe4df2a0a015b7ed0fef050b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f533b37176341d8cc25e8fda155a18e5",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "28bc948eee42b69e0e93",
+    "url": "/recipe-app/static/css/main.9390ba49.chunk.css"
+  },
+  {
+    "revision": "af59a11025415a7ceba8",
+    "url": "/recipe-app/static/js/2.fca3ad06.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/recipe-app/static/js/2.fca3ad06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bc948eee42b69e0e93",
+    "url": "/recipe-app/static/js/main.229c5ed5.chunk.js"
+  },
+  {
+    "revision": "41f3d072ce4105012ff3",
+    "url": "/recipe-app/static/js/runtime-main.27c717ce.js"
+  },
+  {
+    "revision": "744b65722c318dacb52aba6e8390fda3",
+    "url": "/recipe-app/static/media/chinchilla.744b6572.jpg"
+  }
+]);
