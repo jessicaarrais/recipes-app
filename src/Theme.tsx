22ef67a0f0352ea33193885580069758b465656c
Jessica Arrais <jessicalarrais@gmail.com>
2020-09-29 18:38:15 -0700

    code commented removed

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -14,20 +14,3 @@ export default createMuiTheme({
     },
   },
 });
-
-// primary?: PaletteColorOptions;
-// secondary?: PaletteColorOptions;
-// error?: PaletteColorOptions;
-// warning?: PaletteColorOptions;
-// info?: PaletteColorOptions;
-// success?: PaletteColorOptions;
-// type?: PaletteType;
-// tonalOffset?: PaletteTonalOffset;
-// contrastThreshold?: number;
-// common?: Partial<CommonColors>;
-// grey?: ColorPartial;
-// text?: Partial<TypeText>;
-// divider?: string;
-// action?: Partial<TypeAction>;
-// background?: Partial<TypeBackground>;
-// getContrastText?: (background: string) => string;
