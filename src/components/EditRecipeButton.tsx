5acc263ff2ee07d8103846f21520b09d519cc585
Jessica Arrais <jessicalarrais@gmail.com>
2020-11-03 13:52:29 -0800

    history string adjustment

--- src/components/EditRecipeButton.tsx
+++ src/components/EditRecipeButton.tsx
@@ -19,9 +19,7 @@ function EditRecipeButton(props: Props) {
       onClick={(event) => {
         event.preventDefault();
         history.push(
-          `/${'editing'}/cookbook/${props.cookbookId}/recipe/${props.recipeTitle}/${
-            props.recipeId
-          }`
+          `/editing/cookbook/${props.cookbookId}/recipe/${props.recipeTitle}/${props.recipeId}`
         );
       }}
     >
